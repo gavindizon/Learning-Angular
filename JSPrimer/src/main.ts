4c51251b67b20b56e8fc331a7955c3d8d26c7f69
GavinRaineDizon <60138634+GavinRaineDizon@users.noreply.github.com>
2020-05-29 18:30:29 +0800

    made two

---
+++ JSPrimer/src/main.ts
@@ -0,0 +1,22 @@
+class MyClass {
+  
+  constructor(name, weather) {
+    this.name = name;
+    this._weather = weather;
+  }
+  
+  set weather(value) {
+    this._weather = value;
+  }
+  
+  get weather() {
+    return `Today is ${this._weather}`;
+  }
+  printMessages() {
+    console.log("Hello " + this.name + ". ");
+    console.log(this.weather);
+  }
+}
+
+let myData = new MyClass("Adam", "sunny");
+myData.printMessages();
